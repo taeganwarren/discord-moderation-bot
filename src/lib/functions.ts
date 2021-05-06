69e01f1314364c1f55b1499e17fe238be9bbbd48
taegan <warretaega@gmail.com>
2021-05-06 00:20:30 -0700

    Update functions.ts

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -26,19 +26,6 @@ const get_member = (message: Message, toFind: string, return_author: boolean) =>
     return target;
 }
 
-const get_channel = (message: Message, toFind: string) => {
-    // let target = message.guild?.channels.cache.get(toFind);
-    // if (!target && message.mentions.channels) {
-    //     target = message.mentions.channels.first();
-    // }
-    // if (!target) {
-    //     target = message.guild?.members.cache.find(member => {
-    //         return member.displayName.toLowerCase().includes(toFind) ||
-    //             member.user.tag.toLowerCase().includes(toFind);
-    //     });
-    // }
-}
-
 // Formats a date for string output
 const format_date = (date: Date) => {
     return new Intl.DateTimeFormat('en-US').format(date);
@@ -46,6 +33,5 @@ const format_date = (date: Date) => {
 
 export { 
     get_member,
-    get_channel,
     format_date
 }
