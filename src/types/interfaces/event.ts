42348bb398b827172e86aadb6c3c08b457f4a5c3
taegan <warretaega@gmail.com>
2021-05-12 18:51:45 -0700

    Update event.ts

--- src/types/interfaces/event.ts
+++ src/types/interfaces/event.ts
@@ -1,14 +1,9 @@
 // Project imports
 import { Bot } from '../../classes/bot';
 
-// Execute function interface
-export interface Execute {
-    (bot: Bot, ...args: any): Promise<void>
-}
-
 // Event interface
 export interface Event {
     name: string,
     once: boolean,
-    execute: Execute
+    execute(bot: Bot, ...args: any): Promise<void>
 }
